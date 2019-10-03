b0344a92982132692c6567a6edf26e5234035a44
Hongyu Wang <hongyuw2@illinois.edu>
2019-10-02 22:50:48 -0500

    [HW2.0] deleted nodejs version

--- main/scraper.js
+++
@@ -1,6 +0,0 @@
-var request = require('request');
-
-request('https://en.wikipedia.org/wiki/Morgan_Freeman', { json: true }, function (error, response, body) {
-    console.log(JSON.parse(body));
-//   console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
-});
